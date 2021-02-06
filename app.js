da0f374548ed246dd32d6e00947f204e5521758c
cl1v3287 <74298593+cl1v3287@users.noreply.github.com>
2021-02-06 15:27:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var whatsHappening = document.getElementById("whats-happening");
+var howToReduce = document.getElementById("how-to-reduce");
+var whatsHappeningToOthers = document.getElementById("whats-happening-to-others");
+var helplineUrls = document.getElementById("helpline-urls");
+var details = document.getElementById("details");
+
+// whatsHappening.onclick = () =>{
+//     alert("done");
+// }
